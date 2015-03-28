a7bc3177abc2ddb34683f4fb4c6542b87dbeedbd
CoderDojo <CoderDojo@ecm-csse2124-x.uniwa.uwa.edu.au>
2015-03-28 14:07:11 +0800

    Added commands /makehungry and /flyingpig

--- src/scarymonsters/ScaryMonsters.java
+++ src/scarymonsters/ScaryMonsters.java
@@ -1,11 +1,3 @@
-/***
- * Excerpted from "Learn to Program with Minecraft Plugins, CanaryMod Edition",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/ahmine2 for more book information.
-***/
 package scarymonsters;
 
 import net.canarymod.plugin.Plugin;
@@ -16,21 +8,37 @@ import net.canarymod.chat.MessageReceiver;
 import net.canarymod.api.entity.living.humanoid.Player;
 import net.canarymod.api.entity.living.monster.Spider;
 import net.canarymod.api.entity.living.monster.Creeper;
+import net.canarymod.api.entity.living.animal.Pig;
+import net.canarymod.api.entity.living.animal.Bat;
 import net.canarymod.api.world.position.Location;
 import net.canarymod.api.factory.EntityFactory;
 import net.canarymod.api.factory.PotionFactory;
-//import net.canarymod.api.potion.PotionEffect;
-//import net.canarymod.api.potion.PotionEffectType;
+import net.canarymod.api.potion.PotionEffect;
+import net.canarymod.api.potion.PotionEffectType;
 import net.canarymod.api.entity.EntityType;
 import net.canarymod.api.entity.living.EntityLiving;
 import com.pragprog.ahmine.ez.EZPlugin;
 
 public class ScaryMonsters extends EZPlugin {
 
-  @Command(aliases = { "scarymonsters" },
+  @Command(aliases = { "makehungry" },
+            description = "/makehungry",
+            permissions = { "" },
+            toolTip = "/makehungry")
+  public void bfcCommand(MessageReceiver caller, String[] args) {
+    if (caller instanceof Player) {
+	Player who = Canary.getServer().getPlayer(args[1]);
+//	Canary.instance().getServer().broadcastMessage(args[1]);
+	who.setHunger(0);
+    }
+}
+
+
+
+  @Command(aliases = { "creeperjockey" },
             description = "Pure. Terror.",
             permissions = { "" },
-            toolTip = "/scarymonsters")
+            toolTip = "/creeperjockey")
   public void fcCommand(MessageReceiver caller, String[] args) {
     if (caller instanceof Player) { 
       Player me = (Player)caller;
@@ -48,5 +56,29 @@ public class ScaryMonsters extends EZPlugin {
 //		Integer.MAX_VALUE, 1);
     //  spider.addPotionEffect(potion);
     }
+}
+
+  @Command(aliases = { "flyingpig" },
+            description = "Lovely pigs.Ban factory farming!",
+            permissions = { "" },
+            toolTip = "/flyingpig")
+  public void afcCommand(MessageReceiver caller, String[] args) {
+    if (caller instanceof Player) {
+      Player me = (Player)caller;
+      Location loc = me.getLocation();
+      loc.setY(loc.getY());
+
+      EntityFactory factory = Canary.factory().getEntityFactory();
+      EntityLiving bat = factory.newEntityLiving(EntityType.BAT, loc);
+      EntityLiving pig = factory.newEntityLiving(EntityType.PIG, loc);
+      bat.spawn(pig);
+
+      PotionFactory potfact = Canary.factory().getPotionFactory();
+      PotionEffect potion =
+      potfact.newPotionEffect(PotionEffectType.INVISIBILITY,
+              Integer.MAX_VALUE, 1);
+      bat.addPotionEffect(potion);
+    }
+
   }
 }
